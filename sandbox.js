28fa2cf44883758e0e9fead9caf930bcc5e43018
Seerat-Shaikh <129963092+Seerat-Shaikh@users.noreply.github.com>
2023-06-19 22:07:34 +0500

    Js

--- sandbox.js
+++ sandbox.js
@@ -14,7 +14,7 @@ request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
 request.send();*/
 
 // === Callback Functions
-const getTodos = (callback) => {
+/*const getTodos = (resource, callback) => {
   const request = new XMLHttpRequest();
 
   request.addEventListener("readystatechange", () => {
@@ -29,14 +29,14 @@ const getTodos = (callback) => {
   });
 
   //request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-  request.open('GET', 'todos.json');
+  //request.open('GET', 'todos.json');
+  request.open('GET', resource)
   request.send();
 };
 
-console.log(1);
-console.log(2);
 
 // This is Asynchronous code which is started now & will end up later
+
 getTodos((err, data) => {
     console.log('callback fired');
     if(err){
@@ -46,5 +46,45 @@ getTodos((err, data) => {
     }
 });
 
-console.log(3);
-console.log(4);
+// === Callback Hell ===
+getTodos('todos/luigi.json', (err, data) => {
+  console.log(data);
+  getTodos('todos/mario.json', (err, data) => {
+    console.log(data);
+    getTodos('todos/shaun.json', (err, data) => {
+      console.log(data);
+    });
+  });
+});*/
+
+// === Promises ===
+const getTodos = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('error getting resource');
+      }
+    });
+
+    request.open("GET", resource);
+    request.send();
+  });
+};
+
+// === Chaining Promises ===
+getTodos('todos/luigi.json').then(data => {
+  console.log('promise 1 resolved:', data);
+  return getTodos('todos/mario.json');
+}).then(data => {
+  console.log('promise 2 resolved:', data);
+  return getTodos('todos/shaun.json');
+}).then(data => {
+  console.log('promise 3 resolved:', data);
+}).catch((err) => {
+  console.log('promise rejected:', err);
+});
